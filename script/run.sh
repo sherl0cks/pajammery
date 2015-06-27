98f464e7e52dac0e7cf53f3a23bb6fbd261dc9fb
Justin Holmes <justinmichaelholmes@gmail.com>
2015-06-27 00:05:24 -0400

    updating run.sh to use docker hub

--- script/run.sh
+++ script/run.sh
@@ -1,1 +1,4 @@
-docker run -d -p 80:80 pajammery/pajammery
+docker stop pajammery
+docker rm pajammery
+docker pull sherl0cks/pajammery
+docker run --name pajammery -d -p 80:80 docker.io/sherl0cks/pajammery:latest

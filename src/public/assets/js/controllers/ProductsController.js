3c6a9da25d3ff3e07ac1ed5bb8d445e35eb65e29
Justin Holmes <justinmichaelholmes@gmail.com>
2015-07-12 23:59:15 -0400

    fixing uris

--- src/public/assets/js/controllers/ProductsController.js
+++ src/public/assets/js/controllers/ProductsController.js
@@ -1,23 +1,32 @@
 app.controller('ProductsController', [ '$scope', function($scope) {
 
 	$scope.products = [ {
-		id : '0B2MVPEYb3KUxSmUzQV9mbGx1SkU'
+		id : '0B2MVPEYb3KUxSmUzQV9mbGx1SkU',
+		uri : 'https://29a8cbe6dc8003630fd575516f1986d1cdcfcb43-www.googledrive.com/host/0B2MVPEYb3KUxSmUzQV9mbGx1SkU'
 	}, {
-		id : '0B2MVPEYb3KUxWktUb1JQTEZfRTQ'
+		id : '0B2MVPEYb3KUxWktUb1JQTEZfRTQ',
+		uri : 'https://66efb5b5d58f7ab8cf88778dc9ffa2c09f8a676f-www.googledrive.com/host/0B2MVPEYb3KUxWktUb1JQTEZfRTQ'
 	}, {
-		id : '0B2MVPEYb3KUxbTNGTGpSWFVaWEk'
+		id : '0B2MVPEYb3KUxbTNGTGpSWFVaWEk',
+		uri : 'https://1af61046b3355d2a89c2f5b78626e278dcf76cda-www.googledrive.com/host/0B2MVPEYb3KUxbTNGTGpSWFVaWEk'
 	}, {
-		id : '0B2MVPEYb3KUxd1hEdHkyeld6WGc'
+		id : '0B2MVPEYb3KUxd1hEdHkyeld6WGc',
+		uri : 'https://203245fbf184dae0e6fcfeaf5217b5517f680d19-www.googledrive.com/host/0B2MVPEYb3KUxd1hEdHkyeld6WGc'
 	}, {
-		id : '0B2MVPEYb3KUxTUpFS18wdXZTSnM'
+		id : '0B2MVPEYb3KUxTUpFS18wdXZTSnM',
+		uri : 'https://ae0bcd538d6d05f595dd0e156fc96972ad0c36e4-www.googledrive.com/host/0B2MVPEYb3KUxTUpFS18wdXZTSnM'
 	}, {
-		id : '0B2MVPEYb3KUxbzhIT20zUTgtSlU'
+		id : '0B2MVPEYb3KUxbzhIT20zUTgtSlU',
+		uri : 'https://a2ebc7e7698f8f1b46b1a74d4526f19e937b0299-www.googledrive.com/host/0B2MVPEYb3KUxbzhIT20zUTgtSlU'
 	}, {
-		id : '0B2MVPEYb3KUxLS1kUUt1MnNNX1E'
+		id : '0B2MVPEYb3KUxLS1kUUt1MnNNX1E',
+		uri : 'https://3c312893ef30b7dc8185e77c8415a84af1082c85-www.googledrive.com/host/0B2MVPEYb3KUxLS1kUUt1MnNNX1E'
 	}, {
-		id : '0B2MVPEYb3KUxSDRhaVlWQjZpUm8'
+		id : '0B2MVPEYb3KUxSDRhaVlWQjZpUm8',
+		uri : 'https://f5e985c72d55a135bfd0b8cb773da0207a91c9e6-www.googledrive.com/host/0B2MVPEYb3KUxSDRhaVlWQjZpUm8'
 	}, {
-		id : '0B2MVPEYb3KUxaGI5QmY3SUhRY00'
+		id : '0B2MVPEYb3KUxaGI5QmY3SUhRY00',
+		uri : 'https://204d5f07bd969535929b3b21c2f1c4de4d8804b7-www.googledrive.com/host/0B2MVPEYb3KUxaGI5QmY3SUhRY00'
 	} ];
 
 } ]);

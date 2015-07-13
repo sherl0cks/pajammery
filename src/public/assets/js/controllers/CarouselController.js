ed57838492a428e97da7c717eb36a6182d49027f
Justin Holmes <justinmichaelholmes@gmail.com>
2015-07-12 23:25:46 -0400

    changes for v0.2

---
+++ src/public/assets/js/controllers/CarouselController.js
@@ -0,0 +1,17 @@
+app.controller('CarouselController', [ '$scope', function($scope) {
+
+	$scope.features = [ {
+		imageUrl : 'https://12c0ea744604d3af5d7f75d9e1fb0b85fe0a5db4-www.googledrive.com/host/0B2MVPEYb3KUxZUtFOTdOcjV0R0k',
+		imageText : 'woman',
+		heading : 'This a caption and a button',
+		caption : 'Some cool text here'
+	},
+	{
+		imageUrl : 'https://a458237445b3c2a4b4de83180001d024e8fbd22f-www.googledrive.com/host/0B2MVPEYb3KUxdms5SG5kVm5FLXM',
+		imageText : 'man',
+		heading : 'Another caption dude',
+		caption : 'I have nothing to say'
+	} ];
+
+} ]);
+ 
---
+++ src/public/assets/js/controllers/ProductsController.js
@@ -0,0 +1,23 @@
+app.controller('ProductsController', [ '$scope', function($scope) {
+
+	$scope.products = [ {
+		id : '0B2MVPEYb3KUxSmUzQV9mbGx1SkU'
+	}, {
+		id : '0B2MVPEYb3KUxWktUb1JQTEZfRTQ'
+	}, {
+		id : '0B2MVPEYb3KUxbTNGTGpSWFVaWEk'
+	}, {
+		id : '0B2MVPEYb3KUxd1hEdHkyeld6WGc'
+	}, {
+		id : '0B2MVPEYb3KUxTUpFS18wdXZTSnM'
+	}, {
+		id : '0B2MVPEYb3KUxbzhIT20zUTgtSlU'
+	}, {
+		id : '0B2MVPEYb3KUxLS1kUUt1MnNNX1E'
+	}, {
+		id : '0B2MVPEYb3KUxSDRhaVlWQjZpUm8'
+	}, {
+		id : '0B2MVPEYb3KUxaGI5QmY3SUhRY00'
+	} ];
+
+} ]);
